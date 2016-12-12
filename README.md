4d25e81682e2b2cb1f9d3ae63d247de7dc3bfba5
Roland Dubois <rdub80@gmail.com>
2016-12-12 09:36:55 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# aframe-shake2show-component
-Using shake.js to display toggle UI modal
+## aframe-shake2show-component
+
+Tap the side of the smart device (qick shake) as trigger to display a UI Modal for [A-Frame](https://aframe.io) VR.
+
+Integrated Shake.js - JavaScript plugin for mobile web browsers using device accelerometer.
+Author: Alex Gibson
+https://github.com/alexgibson/shake.js
+
+Modified aframe-ui-modal-component
+Author: IdeaSpace
+https://github.https://github.com/IdeaSpaceVR/aframe-ui-modal-component
+
+
+## Properties
+
+| Property       | Description                                            | Default Value |
+| --------       | ------------------------------------------------------ | ------------- |
+| trigger        | Event to make dialog or menu visible.                  | 'shake'       |
+| zpos           | Position the dialog or menu on the z-axis. In meters.  | -0.85         |
+| threshold      | Default quick shake strength threshold                 | 5             |
+
+
+### Usage
+
+#### Browser Installation
+
+Install and use by directly including the [browser files](dist):
+
+```html
+<head>
+  <title>My A-Frame Scene</title>
+  <script src="https://aframe.io/releases/0.3.0/aframe.min.js"></script>
+  <script src="https://rawgit.com/rdub80/aframe-text-component/master/dist/aframe-shake2show-component.min.js"></script>
+</head>
+
+<body>
+  <a-scene>
+    <a-box shake2show visible="false"></a-box>
+  </a-scene>
+</body>
+```
+
+#### NPM Installation
+
+Install via NPM:
+
+```bash
+npm install aframe-shake2show-component
+```
+
+Then register and use.
+
+```js
+require('aframe');
+require('aframe-shake2show-component');
+```
+
+### Dependencies
+
+Your web browser must support the `devicemotion` event for this plugin to work. Shake.js uses built-in feature detection to determine if it can run in your web browser. It will terminate silently on non-supporting browsers.
+
+http://w3c.github.io/deviceorientation/spec-source-orientation.html
+
+
+### Supported web browsers/devices
+
+- iOS Safari 4.2.1 (and above)
+- Android 4.0.3 (default browser)
+- Chrome 41+ for Android
+- Opera Mobile (Android)
+- BlackBerry PlayBook 2.0
+- Firefox for Android
+- FirefoxOS Devices
---
+++ index.js
@@ -0,0 +1,262 @@
+/****** Integrated shake.js plugin:
+/******
+/****** Author: Alex Gibson
+/****** https://github.com/alexgibson/shake.js
+/****** License: MIT license
+/******/
+
+/******/ (function(global, factory) {
+/******/     if (typeof define === 'function' && define.amd) {
+/******/         define(function() {
+/******/             return factory(global, global.document);
+/******/         });
+/******/     } else if (typeof module !== 'undefined' && module.exports) {
+/******/         module.exports = factory(global, global.document);
+/******/     } else {
+/******/         global.Shake = factory(global, global.document);
+/******/     }
+/******/ } (typeof window !== 'undefined' ? window : this, function (window, document) {
+
+/******/     'use strict';
+
+/******/     function Shake(options) {
+/******/         //feature detect
+/******/         this.hasDeviceMotion = 'ondevicemotion' in window;
+
+/******/         this.options = {
+/******/             threshold: 15, //default velocity threshold for shake to register
+/******/             timeout: 1000 //default interval between events
+/******/         };
+
+/******/         if (typeof options === 'object') {
+/******/             for (var i in options) {
+/******/                 if (options.hasOwnProperty(i)) {
+/******/                     this.options[i] = options[i];
+/******/                 }
+/******/             }
+/******/         }
+
+/******/         //use date to prevent multiple shakes firing
+/******/         this.lastTime = new Date();
+
+/******/         //accelerometer values
+/******/         this.lastX = null;
+/******/         this.lastY = null;
+/******/         this.lastZ = null;
+
+/******/         //create custom event
+/******/         if (typeof document.CustomEvent === 'function') {
+/******/             this.event = new document.CustomEvent('shake', {
+/******/                 bubbles: true,
+/******/                 cancelable: true
+/******/             });
+/******/         } else if (typeof document.createEvent === 'function') {
+/******/             this.event = document.createEvent('Event');
+/******/             this.event.initEvent('shake', true, true);
+/******/         } else {
+/******/             return false;
+/******/         }
+/******/     }
+
+/******/     //reset timer values
+/******/     Shake.prototype.reset = function () {
+/******/         this.lastTime = new Date();
+/******/         this.lastX = null;
+/******/         this.lastY = null;
+/******/         this.lastZ = null;
+/******/     };
+
+/******/     //start listening for devicemotion
+/******/     Shake.prototype.start = function () {
+/******/         this.reset();
+/******/         if (this.hasDeviceMotion) {
+/******/             window.addEventListener('devicemotion', this, false);
+/******/         }
+/******/     };
+
+/******/     //stop listening for devicemotion
+/******/     Shake.prototype.stop = function () {
+/******/         if (this.hasDeviceMotion) {
+/******/             window.removeEventListener('devicemotion', this, false);
+/******/         }
+/******/         this.reset();
+/******/     };
+
+/******/     //calculates if shake did occur
+/******/     Shake.prototype.devicemotion = function (e) {
+/******/         var current = e.accelerationIncludingGravity;
+/******/         var currentTime;
+/******/         var timeDifference;
+/******/         var deltaX = 0;
+/******/         var deltaY = 0;
+/******/         var deltaZ = 0;
+
+/******/         if ((this.lastX === null) && (this.lastY === null) && (this.lastZ === null)) {
+/******/             this.lastX = current.x;
+/******/             this.lastY = current.y;
+/******/             this.lastZ = current.z;
+/******/             return;
+/******/         }
+
+/******/         deltaX = Math.abs(this.lastX - current.x);
+/******/         deltaY = Math.abs(this.lastY - current.y);
+/******/         deltaZ = Math.abs(this.lastZ - current.z);
+
+/******/         if (((deltaX > this.options.threshold) && (deltaY > this.options.threshold)) || ((deltaX > this.options.threshold) && (deltaZ > this.options.threshold)) || ((deltaY > this.options.threshold) && (deltaZ > this.options.threshold))) {
+/******/             //calculate time in milliseconds since last shake registered
+/******/             currentTime = new Date();
+/******/             timeDifference = currentTime.getTime() - this.lastTime.getTime();
+
+/******/             if (timeDifference > this.options.timeout) {
+/******/                 window.dispatchEvent(this.event);
+/******/                 this.lastTime = new Date();
+/******/             }
+/******/         }
+
+/******/         this.lastX = current.x;
+/******/         this.lastY = current.y;
+/******/         this.lastZ = current.z;
+
+/******/     };
+
+/******/     //event handler
+/******/     Shake.prototype.handleEvent = function (e) {
+/******/         if (typeof (this[e.type]) === 'function') {
+/******/             return this[e.type](e);
+/******/         }
+/******/     };
+
+/******/     return Shake;
+/******/ }));
+
+/************************************************************************/
+
+/****** modified component from https://github.com/IdeaSpaceVR/aframe-ui-modal-component
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	/**
+	 * UI modal component for A-Frame.
+	 */
+
+	if (typeof AFRAME === 'undefined') {
+	  throw new Error('Component attempted to register before AFRAME was available.');
+	}
+
+	AFRAME.registerComponent('shake2show', {
+
+	    schema: {
+	        trigger: {
+	            default: 'shake'
+	        },
+	        zpos: {
+	            default: -0.85
+	        },
+	        threshold: {
+	        	default: 5
+	        }
+	    },
+
+	    init: function() { 
+
+		    //create a new instance of shake.js.
+		    var myShakeEvent = new Shake({
+		        threshold: this.data.threshold
+		    });
+
+		    // start listening to device motion
+		    myShakeEvent.start();	        
+
+	        var scene = document.querySelector('a-scene');
+    		window.addEventListener(this.data.trigger, this.eventHandler.bind(this));
+
+	        this.cameraEl = document.querySelector('a-entity[camera]');
+
+	        this.yaxis = new THREE.Vector3(0, 1, 0);
+	        this.zaxis = new THREE.Vector3(0, 0, 1);
+
+	        this.pivot = new THREE.Object3D();
+	        this.el.object3D.position.set(0, this.cameraEl.object3D.getWorldPosition().y, this.data.zpos);
+
+	        this.el.sceneEl.object3D.add(this.pivot);
+	        this.pivot.add(this.el.object3D);
+
+	    },
+
+	    eventHandler: function(evt) {
+
+	        if (this.el.getAttribute('visible') === false) {
+
+	            var directionY = this.zaxis.clone();
+	            directionY.applyQuaternion(this.cameraEl.object3D.quaternion);
+	            var ycomponent = this.yaxis.clone().multiplyScalar(directionY.dot(this.yaxis));
+	            directionY.sub(ycomponent);
+	            directionY.normalize();
+
+	            this.pivot.quaternion.setFromUnitVectors(this.zaxis, directionY);
+	            this.pivot.position.copy(this.cameraEl.object3D.getWorldPosition());
+
+	            this.el.setAttribute('visible', true);
+
+	        } else if (this.el.getAttribute('visible') === true) {
+
+	            this.el.setAttribute('visible', false);
+	        }
+
+	    },
+
+	    update: function (oldData) {},
+
+	    remove: function() {}
+
+	});
+
+
+
+
+/***/ }
+/******/ ]);
